e56a9f90def78816b102c7c732177434fd4ddef1
huangduyu <huangduyu@douban.com>
2017-12-14 21:42:33 +0800

    addjust Model and API path

---
+++ aida/aida-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  aida-Bridging-Header.h
+//  aida
+//
+//  Created by bigyelow on 14/12/2017.
+//  Copyright © 2017 bigyelow. All rights reserved.
+//
+
+#ifndef aida_Bridging_Header_h
+#define aida_Bridging_Header_h
+
+
+#endif /* aida_Bridging_Header_h */
